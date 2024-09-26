ef44f9e11fbd473e64c5ae9b507dab8c1d8f2545
Irina Ignateva <ignatyeva_irina@inbox.ru>
2024-09-26 22:09:44 +0300

    fixes after review

--- scripts/index.js
+++ scripts/index.js
@@ -5,31 +5,28 @@ const cardTemplate = document.querySelector('#card-template').content;
 const cardPlacesNode = document.querySelector('.places .places__list');
 
 // @todo: Функция создания карточки
-function createCardOnTemplate(cardTemplate, cardImageSrc, cardTitleText, deleteCardFunction) {
-  const card = cardTemplate.querySelector('.card').cloneNode(true);
-  card.querySelector('.card__image').src = cardImageSrc;
-  card.querySelector('.card__description .card__title').textContent = cardTitleText;
-  card
+function createCardItemOnTemplate(cardTemplate, card, deleteCardFunction) {
+  const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardItem.querySelector('.card__image');
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  cardItem.querySelector('.card__description .card__title').textContent =
+    card.name;
+  cardItem
     .querySelector('.card .card__delete-button')
-    .addEventListener('click', deleteCardFunction);
-  return card;
+    .addEventListener('click', () => deleteCardFunction(cardItem));
+  return cardItem;
 }
 
 function createCards() {
   initialCards.forEach((card) => {
-    const cardItem = createCardOnTemplate(
-      cardTemplate,
-      card.link,
-      card.name,
-      deleteCard
-    );
+    const cardItem = createCardItemOnTemplate(cardTemplate, card, deleteCard);
     cardPlacesNode.append(cardItem);
   });
 }
 
 // @todo: Функция удаления карточки
-function deleteCard() {
-  const cardItem = this.closest('.places__item');
+function deleteCard(cardItem) {
   cardItem.remove();
 }
 
